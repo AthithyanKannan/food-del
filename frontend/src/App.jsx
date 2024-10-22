3164511cc4a61e92a04ed41109d5e4c03b8cd2de
AthithyanKannan <athithyan1314@gmail.com>
2024-10-22 11:12:18 +0530

    Day-2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,20 @@ import { Route,Routes } from 'react-router'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
+    <><div className='app'>
+      <Navbar />
       <Routes>
-        <Route path='/' element = {<Home/>}></Route>
-        <Route path='/cart' element = {<Cart/>}></Route>
-        <Route path='/order' element = {<PlaceOrder/>}></Route>
-        <Route path='/' element = {<Home/>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+        <Route path='/order' element={<PlaceOrder />}></Route>
+        <Route path='/' element={<Home />}></Route>
       </Routes>
     </div>
+    <Footer />
+    </>
   )
 }
 
